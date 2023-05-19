02b582c3562258a47b6531556dac70e357445b54
Zachary Mowatt <zamowatt@gmail.com>
2023-05-18 21:36:37 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, choice) {
+    const lowerCaseChoice = choice.toLowerCase();
+
+    return drivers.filter(driver => driver.toLowerCase() === lowerCaseChoice);
+}
+
+
+function fuzzyMatch(drivers, firstLetter) {
+    const lowerCaseLetter = firstLetter.toLowerCase();
+
+    return drivers.filter(driver => driver.toLowerCase().startsWith(lowerCaseLetter));
+}
+
+
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name === name) ;  
+}
